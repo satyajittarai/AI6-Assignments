9b689e27201e8e526545a531bc23b0d562001afa
satyajittarai <satyajittarai@gmail.com>
2018-08-11 15:20:35 +0530

    Update git_example.py

--- git_example.py
+++ git_example.py
@@ -8,10 +8,11 @@ import numpy as np
 
 def sigmoid(x):
   # enter code below
-  return (1 / (1 + numpy.exp(-x)))
+  return (1 / (1 + np.exp(-x)))
 
 def tanh(x):
   # enter code below
+  retutn ((np.exp(x) - np.exp(-x))/((np.exp(x) + np.exp(-x))
   
 def relu(x):
   # entr code below
